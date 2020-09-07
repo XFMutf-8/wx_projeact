b15bda167cda57b8097d4ec6c3a7a08155de5de5
jingquan@deppon.com <Jq19950518>
2020-09-07 12:18:39 +0800

    1

---
+++ miniprogram/pages/spotList/spotMain.js
@@ -0,0 +1,86 @@
+var spotJson = [
+    {
+        id : 'haidian',
+        dataList : [
+            {
+                id: 'a',
+                name: '校训宝鼎',
+                imageUrl:'https://7365-severless-om62w-1302847609.tcb.qcloud.la/spotImg/%E6%A0%A1%E8%AE%AD%E5%AE%9D%E9%BC%8E.png?sign=231ebe179f3c4c12d4c03b88e2604d3b&t=1599448454'
+            },
+            {
+                id: 'b',
+                name: '法镜',
+                imageUrl:'https://7365-severless-om62w-1302847609.tcb.qcloud.la/spotImg/%E6%B3%95%E9%95%9C.png?sign=634ecca3c0e919715788294bd22090bd&t=1599452196'
+            },
+            {
+                id: 'c',
+                name: '钱端升铜像',
+                imageUrl:'https://7365-severless-om62w-1302847609.tcb.qcloud.la/spotImg/%E9%92%B1%E7%AB%AF%E5%8D%87%E9%9B%95%E5%83%8F.png?sign=52db94eeeb6cb3d759061a920ef403c0&t=1599448748'
+            },
+            {
+                id: 'd',
+                name: '拓荒牛',
+                imageUrl:'https://7365-severless-om62w-1302847609.tcb.qcloud.la/spotImg/%E6%8B%93%E8%8D%92%E7%89%9B.png?sign=d5425ee198b379f0100dcd8f09769326&t=1599448597'
+            },
+            {
+                id: 'e',
+                name: '孔子圣像',
+                imageUrl:'https://7365-severless-om62w-1302847609.tcb.qcloud.la/spotImg/%E5%AD%94%E5%AD%90%E5%9C%A3%E5%83%8F.png?sign=a262b717467912d81ceffd7e6203e881&t=1599452235'
+            },
+            {
+                id: 'f',
+                name: '谢觉哉铜像',
+                imageUrl:'https://7365-severless-om62w-1302847609.tcb.qcloud.la/spotImg/%E8%B0%A2%E8%A7%89%E5%93%89%E9%93%9C%E5%83%8F.png?sign=fefd2515830ae66d421d6f6ba9570e3b&t=1599448625'
+            },
+            {
+                id: 'g',
+                name: '海子石',
+                imageUrl:'https://7365-severless-om62w-1302847609.tcb.qcloud.la/spotImg/%E6%B5%B7%E5%AD%90%E7%9F%B3.png?sign=6d94a856651352eb4b2f893db3003a15&t=1599448514'
+            },
+            {
+                id: 'h',
+                name: '雷洁琼绣像',
+                imageUrl:'https://7365-severless-om62w-1302847609.tcb.qcloud.la/spotImg/%E9%9B%B7%E6%B4%81%E7%90%BC%E7%BB%A3%E5%83%8F.png?sign=817569dedb2abd67cc81a0c152133ad2&t=1599448471'
+            },
+            {
+                id: 'i',
+                name: '彭真雕像',
+                imageUrl:'https://7365-severless-om62w-1302847609.tcb.qcloud.la/spotImg/%E5%BD%AD%E7%9C%9F%E9%9B%95%E5%83%8F.png?sign=b79ff8fcbcae02d91f0a5548bf1b1a95&t=1599448485'
+            }
+        ]
+    },
+    {
+        id : 'changping',
+        dataList : [
+            {
+                id: 'j',
+                name: '1-3号楼',
+                imageUrl:'https://7365-severless-om62w-1302847609.tcb.qcloud.la/spotImg/123%E5%8F%B7%E6%A5%BC.png?sign=048bf76402f9622e789a5ff91d6ec4e5&t=1599448537'
+            },
+            {
+                id: 'k',
+                name: '湖心亭',
+                imageUrl:'https://7365-severless-om62w-1302847609.tcb.qcloud.la/spotImg/%E6%B9%96%E5%BF%83%E4%BA%AD.png?sign=a459a300e169387329b05da40d290534&t=1599448609'
+            },
+            {
+                id: 'l',
+                name: '扬帆雕塑',
+                imageUrl:'https://7365-severless-om62w-1302847609.tcb.qcloud.la/spotImg/%E6%89%AC%E5%B8%86%E9%9B%95%E5%A1%91.png?sign=0c3afd473ce38c4db1c0858b19723355&t=1599448577'
+            },
+            {
+                id: 'm',
+                name: '法治天下碑',
+                imageUrl:'https://7365-severless-om62w-1302847609.tcb.qcloud.la/spotImg/%E6%B3%95%E6%B2%BB%E5%A4%A9%E4%B8%8B%E7%A2%91.png?sign=2f10f0e927c6ed6c7aefb14080be4e2c&t=1599448528'
+            },
+            {
+                id: 'n',
+                name: '钱端升纪念馆',
+                imageUrl:'https://7365-severless-om62w-1302847609.tcb.qcloud.la/spotImg/%E9%92%B1%E7%AB%AF%E5%8D%87%E7%BA%AA%E5%BF%B5%E9%A6%86.png?sign=5cb9790725afd4c1f812648c772a76bd&t=1599452255'
+            }
+        ]
+    }
+]
+
+module.exports = {
+    dataList: spotJson
+}
